e4efd6a332ec961e500e7cbccbfe55b16e941a1b
Henrik Eidlaug <henrik@eidlaug.no>
2024-12-18 10:55:09 +0100

    BalloonSlider

--- src/lessons/BalloonSlider/BalloonSlider.tsx
+++ src/lessons/BalloonSlider/BalloonSlider.tsx
@@ -1,31 +1,75 @@
+import { AnimatedText } from "@/components/AnimatedText";
 import { Container } from "@/components/Container";
 import { hitSlop } from "@/lib/reanimated";
 import { colorShades, layout } from "@/lib/theme";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   Extrapolation,
+  SensorType,
+  clamp,
+  defineAnimation,
   interpolate,
+  measure,
+  useAnimatedReaction,
+  useAnimatedRef,
+  useAnimatedSensor,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
   withSpring,
 } from "react-native-reanimated";
 
+
+
 export function BalloonSliderLesson() {
   const x = useSharedValue(0);
   const knobScale = useSharedValue(0);
+  const balloonScale = useSharedValue(0);
+  const scale = useSharedValue(0);
+  const aRef = useAnimatedRef<View>();
+  const isTouching = useSharedValue(false);
+
+  const progress = useDerivedValue(() => {
+    return x.value / measure(aRef)?.width * 100 || 0
+  });
+
+  const balloonSpringyX = useDerivedValue(() => {
+    return withSpring(x.value);
+  });
+
+  const tapGesture = Gesture.Tap()
+    .maxDuration(100000)
+    .onBegin(() => {
+      scale.value = withSpring(2);
+      balloonScale.value = withSpring(1);
+    })
+    .onEnd(() => {
+      scale.value = withSpring(1);
+      balloonScale.value = withSpring(0);
+    });
 
   const panGesture = Gesture.Pan()
     .averageTouches(true)
+    .onBegin(() => {
+      isTouching.value = true;
+    })
     .onStart(() => {
       knobScale.value = withSpring(1);
     })
     .onChange((ev) => {
-      x.value += ev.changeX;
+      const size = measure(aRef);
+      x.value = clamp((x.value += ev.changeX), 0, size.width);
     })
     .onEnd(() => {
       knobScale.value = withSpring(0);
-    });
+    })
+    .onFinalize(() => {
+      balloonScale.value = 0;
+      isTouching.value = false;
+    })
+
+  const gestures = Gesture.Simultaneous(tapGesture, panGesture);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
@@ -33,7 +77,7 @@ export function BalloonSliderLesson() {
         knobScale.value,
         [0, 1],
         [layout.knobSize / 2, 2],
-        Extrapolation.CLAMP
+        Extrapolation.CLAMP,
       ),
       transform: [
         {
@@ -46,10 +90,116 @@ export function BalloonSliderLesson() {
     };
   });
 
+  const balloonStyle = useAnimatedStyle(() => {
+    return {
+      opacity: balloonScale.value,
+      transform: [
+        { translateX: balloonSpringyX.value },
+        { scale: balloonScale.value },
+        { translateY: interpolate(
+            balloonScale.value,
+            [0, 1],
+            [0, -layout.indicatorSize]
+          ),
+        },
+        {
+          rotate: `${Math.atan2(
+            balloonSpringyX.value - x.value,
+            layout.indicatorSize * 2
+          )}rad`,
+        }
+      ],
+    };
+  });
+
+  function withGravity(userConfig) {
+    "worklet";
+    return defineAnimation(0, () => {
+      "worklet";
+      const config = {
+        acceleration: 9.81,
+        velocity: 0,
+      };
+      Object.assign(config, userConfig);
+      return {
+        onStart: (animation, value, now, previousAnimation) => {
+          animation.current = value;
+          animation.lastTimestamp = previousAnimation?.lastTimestamp ?? now;
+          animation.velocity = previousAnimation?.velocity ?? config.velocity;
+        },
+        onFrame: (animation, now) => {
+          const { lastTimestamp, current, velocity } = animation;
+          const { acceleration, bounds } = config;
+          const delta = (now - lastTimestamp) / 1000;
+          animation.current = current + velocity * delta;
+          animation.velocity =
+            velocity +
+            (acceleration - Math.sign(velocity) * (userConfig.kineticFriction ?? 0)) * delta;
+          animation.lastTimestamp = now;
+      
+          if (bounds) {
+            if (animation.current <= bounds[0]) {
+              animation.current = bounds[0];
+              if (animation.velocity <= 0) {
+                animation.velocity = 0;
+                return true;
+              }
+            } else if (animation.current >= bounds[1]) {
+              animation.current = bounds[1];
+              if (animation.velocity >= 0) {
+                animation.velocity = 0;
+                return true;
+              }
+            }
+          }
+          return false;
+        },
+      };
+    });
+  }
+
+  const GRAVITY = 9.81 * 100;
+
+  const { sensor } = useAnimatedSensor(SensorType.GRAVITY);
+
+  useAnimatedReaction(
+    () => {
+      return isTouching.value ? undefined : GRAVITY * Math.sin(sensor.value.x);
+    },
+    (gravity) => {
+      if (gravity !== undefined) {
+        const size = measure(aRef);
+        x.value = withGravity({
+          bounds: [0, size.width],
+          acceleration: gravity,
+          staticFriction: 800,
+          kineticFriction: 500,
+        });
+      }
+    }
+  );
+
   return (
     <Container>
-      <GestureDetector gesture={panGesture}>
-        <Animated.View style={[styles.knob, animatedStyle]} hitSlop={hitSlop} />
+      <GestureDetector gesture={gestures}>
+        <View style={styles.slider} hitSlop={hitSlop} ref={aRef}>
+          <Animated.View
+            style={[styles.balloon, balloonStyle]}
+            hitSlop={hitSlop}
+          >
+            <View style={styles.textContainer}>
+              <AnimatedText text={progress} style={{ color: "white", fontWeight: "600" }}/>
+            </View>
+          </Animated.View>
+          <Animated.View
+            style={[styles.progress, { width: x }]}
+            hitSlop={hitSlop}
+          />
+          <Animated.View
+            style={[styles.knob, animatedStyle]}
+            hitSlop={hitSlop}
+          />
+        </View>
       </GestureDetector>
     </Container>
   );
@@ -65,5 +215,39 @@ const styles = StyleSheet.create({
     borderColor: colorShades.purple.base,
     position: "absolute",
     left: -layout.knobSize / 2,
+  },
+  slider: {
+    width: "80%",
+    backgroundColor: colorShades.purple.light,
+    height: 5,
+    justifyContent: "center",
+  },
+  progress: {
+    height: 5,
+    backgroundColor: colorShades.purple.dark,
+    position: "absolute",
+  },
+  textContainer: {
+    width: 40,
+    height: 60,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colorShades.purple.base,
+    position: "absolute",
+    top: -layout.knobSize,
+  },
+  balloon: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 4,
+    height: layout.indicatorSize,
+    bottom: -layout.knobSize / 2,
+    borderRadius: 2,
+    backgroundColor: colorShades.purple.base,
+    position: "absolute",
   },
 });
