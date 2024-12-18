77c738e30c9174a9035f480a70710e7d2c7f89e4
Henrik Eidlaug <henrik@eidlaug.no>
2024-12-18 10:55:14 +0100

    ScrollAnimation

--- src/lessons/ScrollAnimation/ScrollAnimation.tsx
+++ src/lessons/ScrollAnimation/ScrollAnimation.tsx
@@ -1,28 +1,61 @@
 import { ContactsListHeader } from "@/components/ContactsListHeader";
 import { ContactsListItem } from "@/components/ContactsListItem";
 import { Container } from "@/components/Container";
-import { alphabet, contacts } from "@/lib/mock";
+import { ContactSection, alphabet, contacts } from "@/lib/mock";
 import { hitSlop } from "@/lib/reanimated";
 import { colorShades, layout } from "@/lib/theme";
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import { SectionList, StyleSheet, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   Extrapolation,
+  SharedValue,
+  clamp,
   interpolate,
+  measure,
+  runOnJS,
+  runOnUI,
+  useAnimatedRef,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
   withSpring,
+  withTiming,
 } from "react-native-reanimated";
 import sectionListGetItemLayout from "react-native-section-list-get-item-layout";
 
 type AlphabetLetterProps = {
   index: number;
   letter: string;
+  scrollableIndex: SharedValue<number>;
 };
 
-const AlphabetLetter = ({ index, letter }: AlphabetLetterProps) => {
+const AlphabetLetter = ({
+  index,
+  letter,
+  scrollableIndex,
+}: AlphabetLetterProps) => {
+  const posY = useSharedValue(0);
+  const styles = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        scrollableIndex.value,
+        [index - 1, index, index + 1],
+        [0.5, 1, 0.5],
+        Extrapolation.CLAMP
+      ),
+      transform: [
+        {
+          scale: interpolate(
+            scrollableIndex.value,
+            [index - 2, index, index + 2],
+            [1, 1.5, 1],
+            Extrapolation.CLAMP
+          ),
+        },
+      ],
+    };
+  });
   return (
     <Animated.View
       style={[
@@ -32,6 +65,7 @@ const AlphabetLetter = ({ index, letter }: AlphabetLetterProps) => {
           justifyContent: "center",
           flexDirection: "row",
         },
+        styles,
       ]}>
       <Animated.Text
         style={[
@@ -51,6 +85,8 @@ const AlphabetLetter = ({ index, letter }: AlphabetLetterProps) => {
 export function ScrollAnimationLesson() {
   const y = useSharedValue(0);
   const isInteracting = useSharedValue(false);
+  const scrollableIndex = useSharedValue(0);
+  const activeScrollIndex = useSharedValue(0);
   const knobScale = useDerivedValue(() => {
     return withSpring(isInteracting.value ? 1 : 0);
   });
@@ -62,17 +98,70 @@ export function ScrollAnimationLesson() {
     });
   }, []);
 
+  const alphabetRef = useAnimatedRef<View>();
+  const scrollViewRef = useRef<SectionList>(null);
+
+  const snapIndicatorTo = (index: number) => {
+    runOnUI(() => {
+      if (scrollableIndex.value === index || isInteracting.value) {
+        return;
+      }
+
+      const alphabetLayout = measure(alphabetRef);
+      if (!alphabetLayout) {
+        return;
+      }
+      const snapBy =
+        (alphabetLayout.height - layout.knobSize) / (alphabet.length - 1);
+      const snapTo = index * snapBy;
+      y.value = withTiming(snapTo);
+      scrollableIndex.value = withTiming(index);
+    })();
+  };
+
+  const scrollToLocation = (index: number) => {
+    scrollViewRef.current?.scrollToLocation({
+      itemIndex: 0,
+      sectionIndex: index,
+      animated: false,
+    })
+  }
+
   const panGesture = Gesture.Pan()
     .averageTouches(true)
     .onBegin(() => {
       isInteracting.value = true;
     })
     .onChange((ev) => {
-      // take into account the knob size
-      y.value += ev.changeY;
+      const alphabetLayout = measure(alphabetRef);
+      if (!alphabetLayout) {
+        return;
+      }
+      y.value = clamp(
+        (y.value += ev.changeY),
+        alphabetLayout.y, // take into account the knob size
+        alphabetLayout.height - layout.knobSize
+      );
+      // This is snapTo by the same interval. This will snap to the nearest
+      // letter based on the knob position.
+      const snapBy =
+        (alphabetLayout.height - layout.knobSize) / (alphabet.length - 1);
+
+      scrollableIndex.value = y.value / snapBy;
+      const snapToIndex = Math.round(scrollableIndex.value);
+
+      runOnJS(scrollToLocation)(snapToIndex);
+
+      // Ensure that we don't trigger scroll to the same index.
+      if (snapToIndex === activeScrollIndex.value) {
+        return;
+      }
+
+      // This is to avoid triggering scrolling to the same index.
+      activeScrollIndex.value = snapToIndex;
     })
     .onEnd(() => {
-      y.value = withSpring(0);
+      runOnJS(snapIndicatorTo)(activeScrollIndex.value);
     })
     .onFinalize(() => {
       isInteracting.value = false;
@@ -101,6 +190,7 @@ export function ScrollAnimationLesson() {
     <Container centered={false}>
       <View style={{ flex: 1 }}>
         <SectionList
+          ref={scrollViewRef}
           contentContainerStyle={{ paddingHorizontal: layout.spacing * 2 }}
           stickySectionHeadersEnabled={false}
           // @ts-ignore
@@ -111,6 +201,17 @@ export function ScrollAnimationLesson() {
           }}
           renderItem={({ item }) => {
             return <ContactsListItem item={item} />;
+          }}
+          onViewableItemsChanged={({ viewableItems }) => {
+            const half = Math.floor(viewableItems.length / 2);
+            const section = viewableItems[half]?.section;
+
+            if (!section) {
+              return;
+            }
+
+            const { index } = section as ContactSection;
+            snapIndicatorTo(index);
           }}
         />
         <View
@@ -127,6 +228,7 @@ export function ScrollAnimationLesson() {
             />
           </GestureDetector>
           <View
+            ref={alphabetRef}
             style={{
               transform: [{ translateX: -layout.indicatorSize / 4 }],
               flex: 1,
@@ -136,7 +238,12 @@ export function ScrollAnimationLesson() {
             pointerEvents='box-none'>
             {[...Array(alphabet.length).keys()].map((i) => {
               return (
-                <AlphabetLetter key={i} letter={alphabet.charAt(i)} index={i} />
+                <AlphabetLetter
+                  key={i}
+                  letter={alphabet.charAt(i)}
+                  index={i}
+                  scrollableIndex={scrollableIndex}
+                />
               );
             })}
           </View>
